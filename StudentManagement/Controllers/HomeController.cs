2e5126aea628e60fc54d8eb9e47894fac39fe157
Nadul_Laknidu <nadullaknidu7@gmail.com>
2025-11-14 20:13:56 +0530

    Update ResponseCache duration in Error action

--- StudentManagement/Controllers/HomeController.cs
+++ StudentManagement/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace StudentManagement.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 1, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
