0b3b92f0c89febba702294070276db8a93cc28f6
Nadul_Laknidu <nadullaknidu7@gmail.com>
2025-11-14 20:13:14 +0530

    Fix typo in HomeController class definition

--- StudentManagement/Controllers/HomeController.cs
+++ StudentManagement/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using StudentManagement.Models;
 
 namespace StudentManagement.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controll
     {
         private readonly ILogger<HomeController> _logger;
 
